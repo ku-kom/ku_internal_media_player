17107ca8b4eb50c90784d8d546a1a1b672fd5b66
nanna-dk <nanna@e-nanna.dk>
2022-07-12 09:22:05 +0200

    Setup

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,6 @@
 #
 
 CREATE TABLE tt_content (
- ku_internal_media_player_autoplay int(11) unsigned DEFAULT '0',
- ku_internal_media_player_loop int(11) unsigned DEFAULT '0',
+    ku_internal_media_player_autoplay int(11) unsigned DEFAULT '0',
+    ku_internal_media_player_loop int(11) unsigned DEFAULT '0',
 );
